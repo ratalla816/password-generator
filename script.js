9398d2c5b966402f7db88075cfebace75634bf88
commander <rob.atalla@gmail.com>
2021-07-04 11:30:08 -0500

    added arrays and variables

--- script.js
+++ script.js
@@ -27,12 +27,11 @@ var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-// When the user answers each prompt, their input should be validated and at least one character type should be selected.
-
-// When all prompts are answered a password is generated that matches the selected criteria.
-
-// The newly generated password is displayed in an alert or written to the page.
-
+var confirmLength = "";
+var confirmSpecialCharacter;
+var confirmNumericCharacter;
+var confirmLowerCase;
+var confirmUpperCase;
 
 // function to generate a random numeric value
 var randomNumber = function(min, max) {
@@ -40,6 +39,14 @@ var randomNumber = function(min, max) {
 
   return value;
 };
+// When the user answers each prompt, their input should be validated and at least one character type should be selected.
+
+// When all prompts are answered a password is generated that matches the selected criteria.
+
+// The newly generated password is displayed in an alert or written to the page.
+
+
+
 
 
 

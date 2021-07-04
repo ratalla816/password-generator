835fc13695afe00d212d78a9335d411787b7d829
commander <rob.atalla@gmail.com>
2021-07-04 15:44:12 -0500

    initialize prompt sequence

--- script.js
+++ script.js
@@ -6,6 +6,24 @@
 var generateBtn = document.querySelector("#generate");
 
 
+// Initialize prompt sequence
+function generatePassword() {
+
+// When prompted to choose the length of the criteria, the user is able to select between 8 and 128 characters.    
+
+//Prompt to choose password length
+var pwdLength = (prompt("Please specify password length. Number of characters must be between 8 and 128."));
+ 
+while(pwdLength <= 7 || pwdLength >= 129) {
+    alert("Length must be between 8 and 128 characters. Please try again");
+    var pwdLength = (prompt("Please specify password length. Number of characters must be between 8 and 128."));
+    } 
+    
+    // Confirm to user how many characters their password will contain  
+    alert(`Thank you, your password will contain ${pwdLength} characters`);
+    
+// The user is prompted to confirm whether or not they wish to include lowercase, uppercase, numeric, and/or special characters.
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -18,26 +36,8 @@ function writePassword() {
 
 // When the user is prompted for password criteria, they are able to select which criteria to include in the password.
 
-// ARRAYS ARE NEEDED! 
 
 
-var confirmLength = "";
-
-
-// Prompt for password length
-var confirmLength = (prompt("Please specify password length. Number of characters must be between 8 and 128.");
-
-// if they select < 8 or > 128 then prompt again
-while(confirmLength <= 8 || confirmLength >= 128) {
-	alert("Length must be between 8 and 128 characters. Please try again");
-	var confirmLength = (prompt("Please specify password length. Number of characters must be between 8 and 128."));
-	} 
-
-// prompts for other values
-var confirmSpecialCharacter = confirm("Please click OK to include special characters, otherwise click CANCEL");
-var confirmNumericCharacter = confirm("Please click OK to include numeric characters, otherwise click CANCEL");    
-var confirmLowerCase = confirm("Please click OK to include lowercase letters, otherwise click CANCEL");
-var confirmUpperCase = confirm("Please click OK to include uppercase letters, otherwise click CANCEL");
 
 
 // function to generate a random numeric value

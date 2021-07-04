33e89b59d31c9160a84127003494d7efc45ddf39
commander <rob.atalla@gmail.com>
2021-07-04 16:10:51 -0500

    getPassword

--- script.js
+++ script.js
@@ -24,47 +24,41 @@ while(pwdLength <= 7 || pwdLength >= 129) {
     
 // The user is prompted to confirm whether or not they wish to include lowercase, uppercase, numeric, and/or special characters.
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+var characterTypes = [];
 
-  passwordText.value = password;
-}
-
-// When the user answers each prompt, their input should be validated and at least one character type should be selected.
-
-// When the user is prompted for password criteria, they are able to select which criteria to include in the password.
-
-
-
-
-
-// function to generate a random numeric value
-for (var i = 0; i < confirmLength; i) {
-  randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+ // Confirm character choices - verify in console log
+var lowerCase = confirm("Please click OK to include lowercase letters, otherwise click CANCEL");
+  if (lowerCase === true) {
+    characterTypes.push(0);
   }
- return randomPassword;
-};
+  console.log(lowerCase);
 
-// When the "Generate Password" button is clicked the user is presented with a series of prompts requesting password criteria.
+var upperCase = confirm("Please click OK to include uppercase letters, otherwise click CANCEL");
+  if (upperCase === true) {
+    characterTypes.push(1);
+  }
+  console.log(upperCase);
 
-// When the user is prompted for password criteria, they are able to select which criteria to include in the password.
+var numeric = confirm("Please click OK to include numbers, otherwise click CANCEL");
+  if (numeric === true) {
+    characterTypes.push(2);
+  }
+  console.log(numeric);
 
-// When prompted to choose the length of the criteria, the user is able to select between 8 and 128 characters.
+var special = confirm("Please click OK to include special characters, otherwise click CANCEL");
+  if (special === true) {
+    characterTypes.push(3);
+  }
+  console.log(special);
 
-// The user is prompted to confirm whether or not they wish to include lowercase, uppercase, numeric, and/or special characters.
+  console.log(characterTypes);
 
-// When the user answers each prompt, their input should be validated and at least one character type should be selected.
+var getPassword = "";
 
-// When all prompts are answered a password is generated that matches the selected criteria.
 
-// The newly generated password is displayed in an alert or written to the page.
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
-
-
-
+}
